5d2cebc35b2d17ba01ea60c698dfb7fbfe947b9d
Dyan Galih <dyan.galih@gmail.com>
2020-09-18 23:11:17 +0700

    Add Model Trait to get all default column

---
+++ src/Traits/ModelTrait.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ */
+
+namespace WebAppId\Lazy\Traits;
+
+use Illuminate\Support\Facades\Cache;
+
+/**
+ * @author: Dyan Galih<dyan.galih@gmail.com>
+ * Date: 18/09/2020
+ * Time: 09.09
+ * Class BaseModel
+ * @package WebAppId\Lazy\Traits
+ */
+trait ModelTrait
+{
+    public function getAllColumn(bool $isFresh = false)
+    {
+        if ($isFresh) {
+            Cache::forget($this->getTable());
+        }
+
+        if (!Cache::has($this->getTable())) {
+            {
+                $columns = $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable());
+                $newColumns = [];
+                foreach ($columns as $column) {
+                    $newColumns[$column] = $this->getTable() . '.' . $column;
+                }
+                Cache::forever($this->getTable(), $newColumns);
+            }
+        }
+        return Cache::get($this->getTable());
+    }
+}
