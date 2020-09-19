a7373e6b9a49cd1966ed461938fb1c6731d86e1c
Dyan Galih <dyan.galih@gmail.com>
2020-09-20 06:04:12 +0700

    Add RepositoryTrait.php

---
+++ src/Traits/RepositoryTrait.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ */
+
+namespace WebAppId\Lazy\Traits;
+
+
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+/**
+ * @author: Dyan Galih<dyan.galih@gmail.com>
+ * Date: 19/09/2020
+ * Time: 09.01
+ * Class RepositoryTrait
+ * @package WebAppId\Lazy\Traits
+ */
+trait RepositoryTrait
+{
+
+    /**
+     * @var array
+     */
+    private $column = [];
+
+    /**
+     * @param Model $model
+     * @param array $joinTable
+     * @return Builder
+     * @throws BindingResolutionException
+     */
+    protected function getJoin(Model $model,
+                               array $joinTable = []): Builder
+    {
+        $builder = $model;
+
+        foreach ($joinTable as $key => $value) {
+            $table = app()->make($value['class']);
+            $this->column[$key] = $table->getColumns();
+            $builder = $builder->join(
+                $table->getTable() . ' as ' . $key,
+                (strpos($value['foreign'], '.') === false ? $model->getTable() . '.' : '') . $value['foreign'],
+                '=',
+                (isset($value['primary']) ? $value['primary'] : $table->getTable() . '.' . $table->getKeyName()),
+                isset($value['type']) ? $value['type'] : 'inner');
+        }
+        return $builder;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getColumn(bool $isAssociative = false): array
+    {
+        $resultColumn =[];
+        foreach ($this->column as $table => $column) {
+            foreach ($column as $key => $value) {
+                if (!isset($resultColumn[$key])) {
+                    $resultColumn[$key] = $value;
+                } else {
+                    $resultColumn[$table . '_' . $key] = $value . ' as ' . Str::singular($table) . '_' . $key;
+                }
+            }
+        }
+
+        if ($isAssociative) {
+            return $resultColumn;
+        } else {
+            return array_values($resultColumn);
+        }
+    }
+}
